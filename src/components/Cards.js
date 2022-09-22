6545ca9b4d50a75a8e217b6558c93b4bf8ef8914
Nick Don <nick.don.1102@gmail.com>
2022-09-22 21:11:31 +1000

    fix buds

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -59,7 +59,7 @@ export default function Example() {
       {people.map((person) => (
         <li
           key={person.email}
-          className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-emerald-600 text-center shadow"
+          className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
         >
           <div className="flex flex-1 flex-col p-8">
             <Image
@@ -81,15 +81,15 @@ export default function Example() {
               </dd>
             </dl>
           </div>
-          <div> 
+          <div>
             <div className="-mt-px flex divide-x divide-gray-200">
               <div className="flex w-0 flex-1">
                 <a
                   href={`mailto:${person.email}`} // images modal
-                  className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-white hover:text-gray-500"
+                  className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-400 hover:text-gray-500"
                 >
                   <EnvelopeIcon
-                    className="h-5 w-5 text-white"
+                    className="h-5 w-5 text-gray-200"
                     aria-hidden="true"
                   />
                   <span className="ml-3">Images</span>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,15 +37,13 @@ export default function Navbar() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <a href="/">
-                    <Image
-                      className="block h-8 w-auto lg:hidden"
-                      src={van}
-                      alt="Your Company"
-                      width={40}
-                      height={40}
-                    />
-                  </a>
+                  <Image
+                    className="block h-8 w-auto lg:hidden"
+                    src={van}
+                    alt="Your Company"
+                    width={40}
+                    height={40}
+                  />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
